82d037d31d6e241f4b81ae4343543c5e338338ff
Saahan <smombasa1989@gmail.com>
2024-06-09 10:31:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="logo-div">
+      <img
+        className="logo-img"
+        src="/img/almanac_logo_soon.jpg"
+        alt="logo"
+        style={{ width: "600px" }}
+      ></img>
+    </div>
+  );
+}
+
+export default App;
